4f145e6099746cfa1ecb317ec3c07030ae897b20
Minh Bui <minh.bui@brreg.no>
2019-11-04 09:33:00 +0100

    Added function for task 2

--- mqtt-task-reporter-bvgs.js
+++ mqtt-task-reporter-bvgs.js
@@ -1,15 +1,16 @@
-function sendInnSvaret(svar, gruppe) {
-  var MQTT_HOST = "test.mosquitto.org"
-  var MQTT_PORT = 8081
-  var MQTT_TOPIC = "bvgs"
+var MQTT_HOST = "test.mosquitto.org"
+var MQTT_PORT = 8081
+var MQTT_TOPIC = "bvgs"
 
-  var resultatEl = document.querySelector("#resultat")
-  var oppgaveEl = document.querySelector("h1")
-  var gruppenavn = gruppe ? ('' + gruppe).trim() : 'Ukjent Gruppe'
-  var oppgavenavn = oppgaveEl ? oppgaveEl.innerHTML.trim() : 'Ukjent Oppgave'
-  
-  var client = new Paho.Client(MQTT_HOST, MQTT_PORT, gruppenavn);
+var resultatEl = document.querySelector("#resultat")
+var oppgaveEl = document.querySelector("h1")
+var gruppenavn = gruppe ? ('' + gruppe).trim() : 'Ukjent Gruppe'
+var oppgavenavn = oppgaveEl ? oppgaveEl.innerHTML.trim() : 'Ukjent Oppgave'
 
+var client = new Paho.Client(MQTT_HOST, MQTT_PORT, gruppenavn);
+
+
+function sendInnSvarOppg1(svar, gruppe) {
   if (resultatEl) {
     resultatEl.innerHTML = svar
   } else {
@@ -17,17 +18,34 @@ function sendInnSvaret(svar, gruppe) {
   }
 
   console.log(svar, gruppe)
-
   client.connect({
     useSSL: true,
+
     onSuccess: function() {
-
       console.log("Koblet til broker")
-      
       var message = new Paho.Message(gruppenavn + " har svart på " + oppgavenavn + " at 1+2 er: " + svar)
-
       message.destinationName = MQTT_TOPIC
+      client.send(message)
+    }
+  })
+}
 
+
+function sendInnSvarOppg2(svar, gruppe) {
+  if (resultatEl) {
+    resultatEl.innerHTML = svar
+  } else {
+    console.error("Kunne ikke finne element med id='resultat'")
+  }
+
+  console.log(svar, gruppe)
+  client.connect({
+    useSSL: true,
+
+    onSuccess: function() {
+      console.log("Koblet til broker")
+      var message = new Paho.Message(gruppenavn + " har svart på " + oppgavenavn + " at bilen til Stine veier: " + svar+ " kg")
+      message.destinationName = MQTT_TOPIC
       client.send(message)
     }
   })
