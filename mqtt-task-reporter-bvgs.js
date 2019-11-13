377c1c21e5c63a9038402b4c0df15f902b082a04
Minh Bui <minh.bui@brreg.no>
2019-11-13 10:37:22 +0100

    Sending a JSON-structure to the feed

--- mqtt-task-reporter-bvgs.js
+++ mqtt-task-reporter-bvgs.js
@@ -1,4 +1,4 @@
-function sendInnSvarOppg1(svar, gruppe) {
+function sendInnSvar(svar, gruppe) {
   var MQTT_HOST = "test.mosquitto.org"
   var MQTT_PORT = 8081
   var MQTT_TOPIC = "bvgs"
@@ -22,42 +22,10 @@ function sendInnSvarOppg1(svar, gruppe) {
 
     onSuccess: function() {
       console.log("Koblet til broker")
-      var message = new Paho.Message(gruppenavn + " har svart på " + oppgavenavn + " at 4+6 er: " + svar)
+      var message = new Paho.Message(JSON.stringify({ gruppenavn, oppgavenavn, svar }))
       message.destinationName = MQTT_TOPIC
       client.send(message)
     }
   })
 }
-
-
-function sendInnSvarOppg2(svar, gruppe) {
-  var MQTT_HOST = "test.mosquitto.org"
-  var MQTT_PORT = 8081
-  var MQTT_TOPIC = "bvgs"
-
-  var resultatEl = document.querySelector("#resultat")
-  var oppgaveEl = document.querySelector("h1")
-  var gruppenavn = gruppe ? ('' + gruppe).trim() : 'Ukjent Gruppe'
-  var oppgavenavn = oppgaveEl ? oppgaveEl.innerHTML.trim() : 'Ukjent Oppgave'
-
-  var client = new Paho.Client(MQTT_HOST, MQTT_PORT, gruppenavn);
-  if (resultatEl) {
-    resultatEl.innerHTML = svar
-  } else {
-    console.error("Kunne ikke finne element med id='resultat'")
-  }
-
-  console.log(svar, gruppe)
-  client.connect({
-    useSSL: true,
-
-    onSuccess: function() {
-      console.log("Koblet til broker")
-      var message = new Paho.Message(gruppenavn + " har svart på " + oppgavenavn + " at bilen til Stine veier: " + svar+ " kg")
-      message.destinationName = MQTT_TOPIC
-      client.send(message)
-    }
-  })
-}
-
 
